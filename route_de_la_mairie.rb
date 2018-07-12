36d33440b5d897480bd428fc3582920db16fc8d8
Alxys <alexys.meunier@gmail.com>
2018-07-12 14:04:26 +0200

    tests

--- route_de_la_mairie.rb
+++ route_de_la_mairie.rb
@@ -1,3 +1,4 @@
+=begin
 
 def get_the_email_of_a_townhal_from_its_webpage
 	require 'rubygems'
@@ -23,4 +24,30 @@ end
 
 get_all_the_urls_of_val_doise_townhalls
 
-#/html/body/table/tbody/tr[3]/td/table/tbody/tr/td[2]/p[2]/object/table/tbody/tr[2]/td/table/tbody/tr/td[1]/p/a[1]
+=end
+
+
+def get_the_email_of_a_townhal_from_its_webpage
+	require 'rubygems'
+	require 'nokogiri'
+	require 'open-uri'
+	doc = Nokogiri::HTML(open("http://annuaire-des-mairies.com/95/vaureal.html"))
+	doc.xpath("/html/body/div/main/section[2]/div/table/tbody/tr[4]/td[2]").each do |node|
+	  puts node.text
+	end
+end
+
+get_the_email_of_a_townhal_from_its_webpage
+
+def get_all_the_urls_of_val_doise_townhalls
+	require 'rubygems'
+	require 'nokogiri'
+	require 'open-uri'
+	doc = Nokogiri::HTML(open("http://annuaire-des-mairies.com/val-d-oise.html"))
+	doc.css("a.lientxt").each do |link|
+  	puts "#{link['href']}"
+	end
+end
+
+get_all_the_urls_of_val_doise_townhalls
+
