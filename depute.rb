29cf5447c4ebb7e258996b34d35c87aca8f8dc4a
laetitia69 <titi-fernandes@hotmail.fr>
2018-07-12 20:54:50 +0200

    fini

--- depute.rb
+++ depute.rb
@@ -4,46 +4,36 @@ require 'open-uri'
 
 
 
-def depute
+
+def depute(url)
+	
+	#ouverture de l'url du ste d'un depute
+	doc = Nokogiri::HTML(open(url))
+
+	#scrapping du mail du depute
+	puts doc.css("dd a.email").map { |mail| mail['href'].sub("mailto:", "")}
+	
+end
 
 
+#############################################################recupere les urls
+def get_all_the_deputes
+	
+	#Ouverture de l'url de l'annuaire des deputes
+	doc = Nokogiri::HTML(open("http://www2.assemblee-nationale.fr/deputes/liste/alphabetique"))
+	doc.css("#deputes-list a").each do |link|
+		#Scrapping de l'url de chaque depute
+		url = "http://www2.assemblee-nationale.fr" + link["href"].to_s
+		#Scrapping du nom du depute
+		depute_name = link.text
+		#Scrapping de l'adresse mail des deputes
+		depute_email = depute(url)
+		#Le tout stoqué dans un hash
+		deputes = {:name => depute_name, :email => depute_email}
+		puts  deputes
 
-
-
-    depute_first_name = [] #création des tableaux de données nom, code postal et site internet
-    depute_last_name = []
-    depute_email = []
-    my_hash = Hash.new #création du hash
-
-    doc = Nokogiri::HTML(open("https://www.nosdeputes.fr/"))
-
-    i = 0
-
-    doc.css('tr td p span a').each do |name|
-        depute_first_name << name.text #scrap des données prénom des députés
-    end
-
-    sites = doc.css('a')
-    lien = sites.each{|link| print link['href'] + " "}
-    lien.each do |site| #scrap des sites internet
-        depute_email << site.text
-    end
-
-    zip = doc.css('a').text #scrap des données code postal
-        zip.each do |z| #scrap des sites internet
-        depute_last_name << z.text
-    end
-
-
-    size = depute_first_name.length
-    while i < size do #boucle pour ranger tableaux dans le hash
-        my_hash[i][:name] = "#{depute_first_name[i]}"
-         my_hash[i][:zipcode] = "#{depute_last_name[i]}"
-        my_hash[i][:website] = "#{depute_email[i]}"
-        i += 1 #évolution indice du hash
-        my_hash << my_hash[i]
-    end
-    puts my_hash
+	end
 
 end
 
+ get_all_the_deputes
